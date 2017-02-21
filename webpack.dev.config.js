11c4db4a2c72c0329da3f18d4df9977484626625
Ignas Strimaitis <strimaitis.ignas@gmail.com>
2017-02-21 02:20:39 +0200

    Keep JSON strings in double quotes

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,17 +2,17 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  devtool: 'eval',
+  devtool: "eval",
 
   entry: [
-    'webpack-hot-middleware/client',
-    './src/index.jsx'
+    "webpack-hot-middleware/client",
+    "./src/index.jsx"
   ],
 
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/'
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+    publicPath: "/"
   },
 
   plugins: [
@@ -22,17 +22,17 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.jsx$|\.es6$|\.js$/,
-        loader: 'babel',
-        include: path.join(__dirname, 'src')
-      },
-      { test: /\.scss?$/,
-        loader: 'style!css!sass',
-        include: path.join(__dirname, 'src', 'styles') },
-      { test: /\.png$/,
-        loader: 'file' },
-      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file'}
+    { test: /\.jsx$|\.es6$|\.js$/,
+      loader: "babel",
+      include: path.join(__dirname, "src")
+    },
+    { test: /\.scss?$/,
+      loader: "style!css!sass",
+      include: path.join(__dirname, "src", "styles") },
+    { test: /\.png$/,
+      loader: "file" },
+    { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      loader: "file"}
     ]
   }
 }
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,30 +2,30 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: "source-map",
 
   entry: [
-    './src/index.jsx'
+    "./src/index.jsx"
   ],
 
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+    publicPath: "/public/"
   },
 
   module: {
     loaders: [
-      { test: /\.jsx$|\.es6$|\.js$/,
-        loader: 'babel',
-        include: path.join(__dirname, 'src') },
-      { test: /\.scss?$/,
-        loader: 'style!css!sass',
-        include: path.join(__dirname, 'src', 'styles') },
-      { test: /\.png$/,
-        loader: 'file' },
-      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file' }
+    { test: /\.jsx$|\.es6$|\.js$/,
+      loader: "babel",
+      include: path.join(__dirname, "src") },
+    { test: /\.scss?$/,
+      loader: "style!css!sass",
+      include: path.join(__dirname, "src", "styles") },
+    { test: /\.png$/,
+      loader: "file" },
+    { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      loader: "file" }
     ]
   }
 }
